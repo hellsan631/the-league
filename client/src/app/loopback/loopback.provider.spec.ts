f48effc5dbb30a5191977c6de6fa9d69a3de4eeb
Mathew Kleppin <matkle414@gmail.com>
2016-05-27 15:53:36 -0500

    comittt

--- client/src/app/loopback/loopback.provider.spec.ts
+++ client/src/app/loopback/loopback.provider.spec.ts
@@ -5,13 +5,39 @@ import {
   expect,
   inject
 } from '@angular/core/testing';
+import { HTTP_PROVIDERS, XHRBackend } from '@angular/http';
 import { LoopbackProvider } from './loopback.provider';
 
 describe('Loopback Service', () => {
-  beforeEachProviders(() => [LoopbackProvider]);
+  beforeEachProviders(() => [
+    LoopbackProvider,
+    HTTP_PROVIDERS,
+    XHRBackend
+  ]);
 
-  it('should ...',
-      inject([LoopbackProvider], (service: LoopbackProvider) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should get a URL property', 
+    inject([XHRBackend, LoopbackProvider], (service: LoopbackProvider) => {
+      expect(service).toBeTruthy();
+    })
+  );
+  
+  it('should format a URL', 
+    inject([XHRBackend, LoopbackProvider], (service: LoopbackProvider) => {
+      var url = service._parseApiUrl('api/members');
+      console.log(url);
+      
+      expect(url).toBeTruthy();
+    })
+  );
+  
+  it('should return the origin url', 
+    inject([XHRBackend, LoopbackProvider], (service: LoopbackProvider) => {
+      var url = service._getOrigin();
+      var expected = 'http://localhost:4200';
+      
+      console.log(url);
+      
+      expect(url === expected).toBeTruthy();
+    })
+  );
 });
