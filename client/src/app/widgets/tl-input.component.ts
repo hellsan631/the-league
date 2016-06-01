609f2cb8ae2104af2eeb54f7cd42102d4b2d5588
hellsan631 <matkle414@gmail.com>
2016-06-01 14:53:55 -0500

    build succeeds

--- client/src/app/widgets/tl-input.component.ts
+++ client/src/app/widgets/tl-input.component.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'app-tl-input',
+  selector: 'tl-input',
   template: `
-    <div></div>
+    <div class="input-field">
+      <input 
+        #input="ngForm"
+        ngControl="input"
+        [type]="type"
+        [attr.name]="name"
+        [attr.max]="max"
+        [attr.maxlength]="maxLength"
+        [attr.min]="min"
+        [attr.minlength]="minLength"
+        [disabled]="disabled"
+        [required]="required"
+        [placeholder]="placeholder" 
+        [(ngModel)]="value" />
+      <label [class.active]="input.dirty" [attr.for]="name">{{label}}</label>
+    </div>
   `,
   styles:[
     `
-      tl-input-component {
+      tl-input {
         
       }
     `
   ]
 })
+
 export class TlInputComponent implements OnInit {
+  @Input() name: string;
+  @Input() label: string;
+  @Input() value: any;
+  @Input() type: string = 'text';
+  @Input() required: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() max: string = null;
+  @Input() maxLength: number = null;
+  @Input() min: string = null;
+  @Input() minLength: number = null;
+  @Input() placeholder: string = null;
 
   constructor() {}
 
--- client/src/typings.d.ts
+++ client/src/typings.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="../typings/browser.d.ts" />
-/// <reference path="../typings/index.d.ts" />
 declare var module: { id: string };
