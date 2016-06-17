1774c7edf5ba2d4c06b1b1aac4cdd2f9e669a498
Whiplash88 <whiplash174@gmail.com>
2016-06-17 17:14:59 -0400

    api/game/thisweek

--- server/models/game.js
+++ server/models/game.js
@@ -1,3 +1,48 @@
-module.exports = function(Game) {
+var moment = require('moment');
+
+module.exports = function (Game) {
+
+  // Get all the games for this week.
+  Game.thisweek = function (cb) {
+
+    // This week Monday
+    var fromDate = moment()
+      .isoWeekday(1)
+      .toISOString()
+      .substring(0, 10);
+
+    // This week Sunday
+    var toDate = moment()
+      .isoWeekday(7)
+      .toISOString()
+      .substring(0, 10);
+
+    var filter = {
+      where: {
+        playedOn: {
+          gt: fromDate,
+          lt: toDate
+        }
+      }
+    };
+
+    Game.find(filter, function (result) {
+      cb(null, result);
+    });
+
+  };
+
+  Game.remoteMethod(
+    'thisweek', {
+      http: {
+        path: '/thisweek',
+        verb: 'get'
+      },
+      returns: {
+        type: 'array',
+        root: true
+      }
+    }
+  );
 
 };
