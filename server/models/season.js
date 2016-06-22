d6387eccf4ef019b8fc605474235ef5d5f608031
Whiplash88 <whiplash174@gmail.com>
2016-06-22 18:02:42 -0400

    Made week in api/seasons/id/weeklygames/week not required

--- server/models/season.js
+++ server/models/season.js
@@ -1,45 +1,16 @@
-module.exports = function(Season) {
+module.exports = function (Season) {
+
+
 
   // Get all the games for this season.
-  Season.weeklygames = function(id, week, cb) {
-
-    var Games = Season.app.models.Game;
-    var filter;
-    var reply = [{}];
-
-    if (week) {
-      filter = {
-        where: {
-          seasonId: id,
-          week: week
-        }
-      };
-    } else {
-      filter = {
-        where: {
-          seasonId: id
-        }
-      };
-    }
-
-    //Doesn't seem to work in a callback, only .then?
-    Games.find(filter)
-    .then(function(result){
-      console.log(result);
-      result.map(function(element){
-        if (!reply[0][element.week]) {
-          reply[0][element.week] = [element];
-        } else {
-          reply[0][element.week].push(element);
-        }
-      });
-
-      cb(null, reply);
-      
-    });
-
-
-    
+  Season.weeklyGamesWeek = function (id, week, cb) {    
+    var filter = {
+      where: {
+        seasonId: id,
+        week: week
+      }
+    };
+    getGames(filter, cb);
   };
 
 
@@ -47,8 +18,8 @@ module.exports = function(Season) {
   // http://localhost:3000/api/Seasons/1/weeklygames/0 --> All games.
   // http://localhost:3000/api/Seasons/1/weeklygames/1 --> Games for week 1.
   Season.remoteMethod(
-    'weeklygames', {
-      accepts:[
+    'weeklyGamesWeek', {
+      accepts: [
         {
           arg: 'id',
           type: 'string',
@@ -57,11 +28,11 @@ module.exports = function(Season) {
         {
           arg: 'week',
           type: 'number',
-          required: true 
+          required: true
         }
       ],
       http: {
-        path: '/:id/weeklygames/:week', 
+        path: '/:id/weeklygames/:week',
         verb: 'get'
       },
       returns: {
@@ -70,5 +41,57 @@ module.exports = function(Season) {
       }
     }
   );
+
+  // Get all the games for this season.
+  Season.weeklyGames = function (id, cb) {
+    var filter = {
+      where: {
+        seasonId: id
+      }
+    };
+    getGames(filter, cb);
+  };
+
+  // IMPORTANT: as soon as you mixin arguments, they are required: true
+  // http://localhost:3000/api/Seasons/1/weeklygames/0 --> All games.
+  // http://localhost:3000/api/Seasons/1/weeklygames/1 --> Games for week 1.
+  Season.remoteMethod(
+    'weeklyGames', {
+      accepts: [
+        {
+          arg: 'id',
+          type: 'string',
+          required: true
+        }
+      ],
+      http: {
+        path: '/:id/weeklygames/',
+        verb: 'get'
+      },
+      returns: {
+        type: 'array',
+        root: true
+      }
+    }
+  );
+
+  function getGames(filter, cb) {
+    var reply = [{}];
+    var Games = Season.app.models.Game;
+    Games.find(filter)
+      .then(function (result) {
+        console.log(result);
+        result.map(function (element) {
+          if (!reply[0][element.week]) {
+            reply[0][element.week] = [element];
+          } else {
+            reply[0][element.week].push(element);
+          }
+        });
+
+        cb(null, reply);
+
+      });
+  }
 
 };
